5103983d8ddf86a17d5ea475f2a95cd13d77685b
Manuel Alfaro <manuel.alfaro1488@gmail.com>
2022-05-01 00:10:54 -0500

    restructurar carpetas y archivos

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 // Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
+const rawdata = fs.readFileSync("./app/explorers.json");
 const explorers = JSON.parse(rawdata);
 
 // Part 2: Get the quantity of explorers names in node
